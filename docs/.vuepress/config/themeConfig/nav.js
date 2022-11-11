bd152de7cec115ccdd5577edb4e1a577e10cc525
willcsw <67531201+willcsw@users.noreply.github.com>
2022-11-11 11:33:47 -0800

    remove 我的博客 from nav.js

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: 'Reveries',link: '/one/',},
+  { text: 'Thoughts',link: '/two/',},
   { text: '留言板', link: '/message-board/' },
-  { text: '我的博客', link: 'https://wiki.eryajf.net' },
+  // { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
