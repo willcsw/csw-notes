d3c0a1e78dc9d97a285e231b072ae94467fe9d9c
willcsw <67531201+willcsw@users.noreply.github.com>
2022-11-11 11:40:07 -0800

    add back three to in  nav.js

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -3,6 +3,7 @@ module.exports = [
   { text: '首页', link: '/' },
   { text: 'Reveries',link: '/one/',},
   { text: 'Thoughts',link: '/two/',},
+  { text: 'three', link: '/three/' },
   { text: '留言板', link: '/message-board/' },
   // { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
