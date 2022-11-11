31a4701addc2352dd197398a416747bcf3d943c0
willcsw <67531201+willcsw@users.noreply.github.com>
2022-11-11 11:52:06 -0800

    revert changes in nav.js

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,8 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'Reveries',link: '/one/',},
-  { text: 'Thoughts',link: '/two/',},
+  { text: 'one',link: '/one/',},
+  { text: 'two',link: '/two/',},
   { text: 'three', link: '/three/' },
   { text: '留言板', link: '/message-board/' },
   // { text: '我的博客', link: 'https://wiki.eryajf.net' },
